d0195c616e615f3d5008115d4d40c6787a443c2d
James Hrisho <james.hrisho@gmail.com>
2015-02-23 10:35:03 -0500

    filtering and react-router

--- app/js/components/tiles.jsx
+++ app/js/components/tiles.jsx
@@ -2,18 +2,26 @@
 var React = require('react/addons');
 var _  = require('lodash');
 var Tile  = require('./tile.jsx');
-
+var repos = require('../repos.json');
+var Router = require('react-router');
 
 module.exports = React.createClass({
+  mixins: [Router.State],
   render: function() {
+    filteredRepos = repos;
+    if (this.getParams().type) {
+      var self = this;
+      filteredRepos = _.filter(repos, _.matches({'language': self.getParams().type}) );
+    }
+    
     return (
       <div className="tiles">
-     { _.map(this.props.projects,function(project){
+     { _.map(filteredRepos,function(project){
       return <Tile 
     name={project.name}
     description={project.description}
     link={project.html_url}
-    logo={project.owner.avatar_url}
+    logo={"https://s3.amazonaws.com/maxwellv5-employers%2F/maxwell.logo-vert-blue.png"}
     tag={project.language}
      />
            })}
